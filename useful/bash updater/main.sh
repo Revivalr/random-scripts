48ff2dbe510d13963d8ec6f652fbbdbfac9b5936
Rockpods <69652659+Rockpods@users.noreply.github.com>
2021-10-30 02:44:02 +0000

    Create main.sh

---
+++ useful/bash updater/main.sh
@@ -0,0 +1,123 @@
+md5local=$(md5sum main.sh | cut -d ' ' -f 1)
+md5localcheck=$(md5sum main.sh | cut -d ' ' -f 1)
+rm md5server.sh
+curl (ENTER RAW VERSION OF SCRIPT HERE) --output (output).sh
+md5server=md5sum | cut -d ' ' -f 1
+rm md5servercheck.sh
+curl (ENTER RAW VERSION OF SCRIPT HERE) --output (output).sh
+md5servercheck=md5sum | cut -d ' ' -f 1
+if [ $md5local == $md5localcheck ]
+then
+  
+else
+    echo 'Error'
+fi
+
+echo "Do you need to update
+
+Yes or No"
+ 
+read update
+
+if [ $update == yes ]
+then
+  md5update() {
+      echo "
+Redownloading the script.
+  "
+    echo
+    echo 'Deleting old update scripts.'
+    echo
+    rm updatescript.sh
+    rm updatescript1.sh
+    rm updatescript2.sh
+    rm updatescript3.sh
+    rm updatescript4.sh
+    rm md5server.sh
+    rm md5servercheck.sh
+    echo
+    echo
+    echo 'Finished deleting old update scripts.'
+    echo
+    echo
+    echo 'Generating md5 checksum for installed script.'
+    echo
+    md5local=$(md5sum main.sh | cut -d ' ' -f 1)
+    md5localcheck=$(md5sum main.sh | cut -d ' ' -f 1)
+    echo
+    echo 'Verifying the md5 checksum for the installed script.'
+    echo
+    if [ $md5local == $md5localcheck ]
+    then
+      echo
+      echo 'The checksum for the installed script has been verified.'
+      echo
+    else
+      errorblank='There was a problem with the md5 checksum generated to tell ig there was an update.'
+      error
+    echo
+    echo 'Generating md5 checksum for the script on Bitbucket.'
+    echo
+    curl (ENTER RAW VERSION OF SCRIPT HERE) --output (output).sh
+    md5server=md5sum | cut -d ' ' -f 1
+    curl (ENTER RAW VERSION OF SCRIPT HERE) --output (output).sh
+    md5servercheck=md5sum | cut -d ' ' -f 1
+    echo 'Verifying the md5 checksum for the script on Bitbucket.'
+    if [ $md5server == $md5servercheck ]
+    then
+      echo
+      echo 'The checksum for the Bitbucket script has been verified.'
+      echo
+    else
+      errorblank='The checksum for the Bitbucket script has not been verified.'
+      error
+    fi
+    echo 'Downloading update script'
+   #example
+   #Add when I upload
+    curl (ENTER RAW UPDATE SCRIPT LINK HERE) --output (OUTPUT).sh
+    curl (ENTER RAW UPDATE SCRIPT LINK HERE) --output (OUTPUT).sh
+    curl (ENTER RAW UPDATE SCRIPT LINK HERE) --output (OUTPUT).sh
+    curl (ENTER RAW UPDATE SCRIPT LINK HERE) --output (OUTPUT).sh
+    curl (ENTER RAW UPDATE SCRIPT LINK HERE) --output (OUTPUT).sh
+    echo 'Verifying updater'
+    md5update1=$(md5sum updatescript.sh | cut -d ' ' -f 1)
+    md5update2=$(md5sum updatescript1.sh | cut -d ' ' -f 1)
+    md5update3=$(md5sum updatescript2.sh | cut -d ' ' -f 1)
+    md5update4=$(md5sum updatescript3.sh | cut -d ' ' -f 1)
+    md5update5=$(md5sum updatescript4.sh | cut -d ' ' -f 1)
+    echo $md5update5
+    if []
+}
+
+
+error() {
+      echo
+      echo '$errorblank'
+      echo
+      echo "
+
+
+!!!READ!!! There has been an error somewhere in the program."
+      echo "Would you like to retry or skip updating?
+0) Retry
+1) Skip"
+      read servermd5error
+      if [ $servermd5error == 0 ] || [ $servermd5error == retry ]
+      then
+        echo 'Retrying the update'
+        md5update
+      else
+        echo 'Skipping update'
+      fi
+    return [n]
+}
+md5update
+  echo "
+Opening update script
+"
+  bash updatescript.sh
+  exit
+  echo '!!!You should never see this!!! If you do press ctrl+c to exit the program and then retry. If this is your third time seeing this please submit a bug report to the Bitbucket.'
+fi
+echo

3e3d80ed83a7a726567483690665e259fc8eaf58
Rockpods <69652659+Rockpods@users.noreply.github.com>
2021-10-30 04:54:02 +0000

    Update ytdlcli.sh

--- useful/youtube/ytdlcli.sh
+++ useful/youtube/ytdlcli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt -y install youtube-dl
-sudo apy -y install ffmpeg
+sudo apt -y install ffmpeg
 askagain() {
 echo 'Would you like to download another video/playlist?
 Yes or No'
@@ -9,10 +9,12 @@ if [ $downloadagain == yes ] || [ $downloadagain == yea ] || [ $downloadagain  =
 then
    echo 'Restarting'
 elif [ $downloadagain == no ] || [ $downloadagain == n ]
+then
    exit
 else
    echo 'Please enter a valid answer.'
    askagain
+fi
 }
 main() {
 echo 'What YouTube video/playlist do you want to download?'
@@ -20,6 +22,8 @@ read download
 echo 'What do you want to call the file?'
 read filename
 youtube-dl $download -o $filename.mkv
+}
+main
 convert() {
 echo 'Do you want to convert the video into an mp3?
 Yes or No'
@@ -29,7 +33,16 @@ then
    echo 'What would you like to call the audio file?'
    read audioname
    ffmpeg -i "$filename.mkv" -vn -ar 44100 -ac 2 -ab 128k -f mp3 "$audioname.mp3"
-   deleteoriginal() {
+elif [ $convert == no ] || [ $convert == n]
+then
+   exit
+else
+   echo 'Please enter a valid answer.'
+   convert
+fi
+}
+convert
+deleteoriginal() {
    echo 'Do you want to delete the original video file?
 Yes or No'
    read deleteoriginal
@@ -38,19 +51,11 @@ Yes or No'
       rm $filename.mkv
       echo 'Original file deleted'
       askagain
-   elif [ $deleteoriginal == no ] || [ $deleteoriginal == n ]
+         elif [ $deleteoriginal == no ] || [ $deleteoriginal == n ]
+   then
       askagain
    else
       deleteoriginal
    fi
 }
 deleteoriginal
-elif [ $convert == no ] || [ $convert == n ]
-   askagain
-else
-   echo 'Please enter a valid answer.'
-   convert
-}
-convert
-}
-main
